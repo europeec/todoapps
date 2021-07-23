b0466ddc4bfd652e0905c6d04149bd692aaa7180
europeec <l.europeec@ya.ru>
2021-07-23 13:20:59 +0300

    init ui project

---
+++ swiftui/todo/todo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Дмитрий Юдин on 23.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui/todo/todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Дмитрий Юдин on 23.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
